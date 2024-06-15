1cf52bac4d73a98448edb7dd1248d356cc93f616
ErShivnandan <ec1912@global.org.in>
2024-06-15 16:53:55 +0530

    Search route added and scrollbar updated

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,15 +1,16 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./Pages/Home"
-import Watch from "./Pages/Watch"
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Watch from "./Pages/Watch";
+import Search from "./Pages/Search";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
       <Route path="/watch/:id" element={<Watch />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
